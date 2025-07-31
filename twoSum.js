0e6502bd32c6216a98904b110e8807195d412aad
Bhima Baskey <bhima.dev26@gmail.com>
2025-07-31 20:45:12 +0530

    all operations

--- twoSum.js
+++ twoSum.js
@@ -3,3 +3,12 @@ const twoSum=(num1,num2)=>{
     return twosum;
 }
 console.log("Sum of two numbers is :",twoSum(5,10));
+const difference=(num1,num2)=>{
+    const diff=num1-num2;
+    return diff;
+};
+console.log("Difference of two numbers is :",difference(10,5));
+const number1=20;
+const number2=30;
+const multiplication=number1*number2;
+console.log("Multiplication of two numbers is :",multiplication);
